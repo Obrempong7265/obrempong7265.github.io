cf61130049549826901f968e6f90e6dd55772ba2
Obrempong7265 <kinsfredaddison@gmail.com>
2026-08-11 08:56:26 +0000

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -6,13 +6,15 @@ const SUPABASE_URL =
     "https://fkcyhqaxsfsnukbeebwu.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY =
-    "sb_publishable_jnR4OJZTppqkeezebYc3PQ_KQyQxusH
+    sb_publishable_jnR4OJZTppqkeezebYc3PQ_KQyQxusH
 
 
-const supabaseClient =
+window.supabaseClient =
     window.supabase.createClient(
         SUPABASE_URL,
         SUPABASE_PUBLISHABLE_KEY
     );
 
-console.log("Video City Supabase connected.");
+console.log(
+    "Video City: Supabase connected."
+);
